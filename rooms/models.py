2c0efdf546e17b81382bf5a75fff944cef5d1e5e
bunnycast <berzzubunny@gmail.com>
2021-12-09 17:19:07 +0900

    3.3 Room model 3 - creating MTM model and admin amenity, facility, house rule

--- rooms/models.py
+++ rooms/models.py
@@ -18,6 +18,29 @@ class AbstractItem(core_models.TimeStampedModel):
 
 class RoomType(AbstractItem):
 
+    """ RoomType Model Definition"""
+
+    pass
+
+
+class Amenity(AbstractItem):
+
+    """ Amenity Model Definition """
+
+    pass
+
+
+class Facility(AbstractItem):
+
+    """ Facility Model Definition """
+
+    pass
+
+
+class HouseRule(AbstractItem):
+
+    """ HouseRule Model Definition """
+
     pass
 
 
@@ -45,11 +68,14 @@ class Room(core_models.TimeStampedModel):
 
     instant_book = models.BooleanField(default=False)
 
-    # room => user ForeignKey
+    # room => user ForeignKey N:1
     host = models.ForeignKey("users.User", on_delete=models.CASCADE)
+    room_type = models.ForeignKey(RoomType, on_delete=models.SET_NULL, null=True)
 
-    # Room <=> RoomType MTM 연결
-    room_type = models.ManyToManyField(RoomType, blank=True)
+    # Room <=> RoomItem 상속받은 모델 (Facility, Amenity, HouseRule) MTM 연결
+    facilities = models.ManyToManyField(Facility, blank=True)
+    amenities = models.ManyToManyField(Amenity, blank=True)
+    house_rules = models.ManyToManyField(HouseRule, blank=True)
 
     # 객체를 전달할 때 표시되는 이림을 변경
     def __str__(self):
