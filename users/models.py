297f8bfe08caaea1c88538e96659e4155350232e
bunnycast <berzzubunny@gmail.com>
2021-12-09 14:58:20 +0900

    2.2 Expand User model 1. creating fields : avatar, gender, birthdate, language, currency, is_superhost 2. install Pillow

--- users/models.py
+++ users/models.py
@@ -3,4 +3,48 @@ from django.db import models
 
 
 class User(AbstractUser):
-    bio = models.TextField(default="")
+    """Custom User Model"""
+
+    # DB에 저장되는 필드명
+    GENDER_MALE = "male"
+    GENDER_FEMALE = "female"
+    GENDER_OTHER = "other"
+
+    # form 에서 보이는 필드명
+    GENDER_CHOICES = (
+        (GENDER_MALE, "Male"),
+        (GENDER_FEMALE, "Female"),
+        (GENDER_OTHER, "Other"),
+    )
+
+    LANGUAGE_ENGLISH = "en"
+    LANGUAGE_KOREA = "kr"
+
+    LANGUAGE_CHOICES = (
+        (LANGUAGE_ENGLISH, "English"),
+        (LANGUAGE_KOREA, "Korea"),
+    )
+
+    CURRENCY_USD = "usd"
+    CURRENCY_KRW = "krw"
+
+    CURRENCY_CHOICES = ((CURRENCY_USD, "USD"), (CURRENCY_KRW, "KRW"))
+
+    # Image field needs Pillow, null : db, blank : form
+    avatar = models.ImageField(null=True, blank=True)
+
+    # custom choices field > don't need migration
+    gender = models.CharField(
+        choices=GENDER_CHOICES, max_length=10, null=True, blank=True
+    )
+    birthdate = models.DateField(null=True)
+    language = models.CharField(
+        choices=LANGUAGE_CHOICES, max_length=2, null=True, blank=True
+    )
+    currency = models.CharField(
+        choices=CURRENCY_CHOICES, max_length=3, null=True, blank=True
+    )
+
+    bio = models.TextField(default="", blank=True)
+
+    is_superHost = models.BooleanField(default=False)
