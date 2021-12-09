2df5dc2da94d8020d5e611d67e8f8c47151cc422
bunnycast <berzzubunny@gmail.com>
2021-12-09 16:01:24 +0900

    3. Room App 1. creating room admin, model 2. creating Core App and TimeStampedModel > abstract model

---
+++ core/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+
+
+class TimeStampedModel(models.Model):
+
+    """Time Stamped Model"""
+
+    created_at = models.DateTimeField()
+    updated_at = models.DateTimeField()
+
+    class Meta:
+        abstract = True
+    # time Stamp 를 사용하는 모든 app 모델에 상속하는 추상 모델 (DB에 등록되지 않음) // migration 필요 없음
