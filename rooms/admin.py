5dc53eae4b631abe0daca53bcb2d985ce802d243
bunnycast <berzzubunny@gmail.com>
2021-12-10 16:00:08 +0900

    Expand Room admin Panel - list_display , list_filter, search_filter

--- rooms/admin.py
+++ rooms/admin.py
@@ -5,7 +5,7 @@ from . import models
 @admin.register(models.RoomType, models.Facility, models.Amenity, models.HouseRule)
 class ItemAdmin(admin.ModelAdmin):
 
-    """ Item Admin Definition """
+    """Item Admin Definition"""
 
     pass
 
@@ -13,14 +13,38 @@ class ItemAdmin(admin.ModelAdmin):
 @admin.register(models.Room)
 class RoomAdmin(admin.ModelAdmin):
 
-    """ Room Admin Definition """
+    """Room Admin Definition"""
 
-    pass
+    # admin > 목록 페이지에서 보여줄 필드를 설정
+    list_display = (
+        "name",
+        "country",
+        "city",
+        "price",
+        "address",
+        "guests",
+        "beds",
+        "bedrooms",
+        "baths",
+        "check_in",
+        "check_out",
+        "instant_book",
+    )
+
+    # admin > 목록 페이지에서 필터링 할 필드를 지정
+    list_filter = (
+        "instant_book",
+        "city",
+        "country",
+    )
+
+    # admin > 목록 페이지에서 검색할 필드 지정 (prefix : ^startswith =iexact @search None)
+    search_fields = ("=city", "^host__username")
 
 
 @admin.register(models.Photo)
 class PhotoAdmin(admin.ModelAdmin):
 
-    """ Photo Admin Definition"""
+    """Photo Admin Definition"""
 
     pass
