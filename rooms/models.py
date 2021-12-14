1dfc374af7f5b3616a40816480674f5f304c6c60
bunnycast <berzzubunny@gmail.com>
2021-12-14 17:07:38 +0900

    3.8. Custom Room Admin Panel - def count_amenities

--- rooms/models.py
+++ rooms/models.py
@@ -18,7 +18,7 @@ class AbstractItem(core_models.TimeStampedModel):
 
 class RoomType(AbstractItem):
 
-    """ RoomType Model Definition"""
+    """RoomType Model Definition"""
 
     pass
 
@@ -29,7 +29,7 @@ class RoomType(AbstractItem):
 
 class Amenity(AbstractItem):
 
-    """ Amenity Model Definition """
+    """Amenity Model Definition"""
 
     pass
 
@@ -39,7 +39,7 @@ class Amenity(AbstractItem):
 
 class Facility(AbstractItem):
 
-    """ Facility Model Definition """
+    """Facility Model Definition"""
 
     pass
 
@@ -49,7 +49,7 @@ class Facility(AbstractItem):
 
 class HouseRule(AbstractItem):
 
-    """ HouseRule Model Definition """
+    """HouseRule Model Definition"""
 
     pass
 
@@ -59,11 +59,13 @@ class HouseRule(AbstractItem):
 
 class Photo(core_models.TimeStampedModel):
 
-    """ Photo model Definition """
+    """Photo model Definition"""
 
     caption = models.CharField(max_length=80)
     file = models.ImageField()
-    room = models.ForeignKey("rooms.Room", on_delete=models.CASCADE)
+    room = models.ForeignKey(
+        "rooms.Room", related_name="photos", on_delete=models.CASCADE
+    )
 
     def __str__(self):
         return self.caption
@@ -71,7 +73,7 @@ class Photo(core_models.TimeStampedModel):
 
 class Room(core_models.TimeStampedModel):
 
-    """ Room Model Definition """
+    """Room Model Definition"""
 
     name = models.CharField(max_length=140)
     description = models.TextField()
@@ -94,13 +96,23 @@ class Room(core_models.TimeStampedModel):
     instant_book = models.BooleanField(default=False)
 
     # room => user ForeignKey N:1
-    host = models.ForeignKey("users.User", on_delete=models.CASCADE)
-    room_type = models.ForeignKey("rooms.RoomType", on_delete=models.SET_NULL, null=True)
+    host = models.ForeignKey(
+        "users.User", related_name="rooms", on_delete=models.CASCADE
+    )
+    room_type = models.ForeignKey(
+        "rooms.RoomType", related_name="rooms", on_delete=models.SET_NULL, null=True
+    )
 
     # Room <=> RoomItem 상속받은 모델 (Facility, Amenity, HouseRule) MTM 연결
-    facilities = models.ManyToManyField("rooms.Facility", blank=True)
-    amenities = models.ManyToManyField("rooms.Amenity", blank=True)
-    house_rules = models.ManyToManyField("rooms.HouseRule", blank=True)
+    facilities = models.ManyToManyField(
+        "rooms.Facility", related_name="rooms", blank=True
+    )
+    amenities = models.ManyToManyField(
+        "rooms.Amenity", related_name="rooms", blank=True
+    )
+    house_rules = models.ManyToManyField(
+        "rooms.HouseRule", related_name="rooms", blank=True
+    )
 
     # 객체를 전달할 때 표시되는 이름을 변경
     def __str__(self):
